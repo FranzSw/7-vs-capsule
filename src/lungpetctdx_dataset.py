0ddc9e6ec7c2363ed715256e2177043bd8e80cbd
tfiedlerdev <tobias.fiedler@student.hpi.de>
2023-01-15 13:02:37 +0100

    WIP work on cropping tumor

--- src/lungpetctdx_dataset.py
+++ src/lungpetctdx_dataset.py
@@ -12,31 +12,40 @@ from typing import Union
 from loading.getUID import *
 
 from pathlib import Path
+
 dataset_path = "/dhc/dsets/Lung-PET-CT-Dx"
 num_classes = 4
-all_class_names = ["Adenocarcinoma", "Small Cell Carcinoma",
-                   "Large Cell Carcinoma", "Squamous Cell Carcinoma"]
+all_class_names = [
+    "Adenocarcinoma",
+    "Small Cell Carcinoma",
+    "Large Cell Carcinoma",
+    "Squamous Cell Carcinoma",
+]
 
 
 def shortId(ext_str: str):
-    return ext_str.replace('Lung_Dx-', '')
+    return ext_str.replace("Lung_Dx-", "")
 
 
 def get_xml_files(subject_ids):
-    ids_folders = list(map(lambda s: (s, os.path.join(
-        dataset_path, 'labels', shortId(s))), subject_ids))
+    ids_folders = list(
+        map(
+            lambda s: (s, os.path.join(
+                dataset_path, "labels", shortId(s))), subject_ids
+        )
+    )
     return list(filter(lambda tuple: os.path.exists(tuple[1]), ids_folders))
 
 
 def load_files_for_subject(subject_id: str) -> list[tuple[str, str, str, list[int]]]:
-    print(f'Loading subject {subject_id}')
-    folder = os.path.join(dataset_path, 'labels', shortId(subject_id))
+    print(f"Loading subject {subject_id}")
+    folder = os.path.join(dataset_path, "labels", shortId(subject_id))
     if not os.path.exists(folder):
         return []
     annotations = XML_preprocessor(folder, num_classes=num_classes).data
 
     available_dicom_files = getUID_path(
-        os.path.join(dataset_path, 'data', subject_id))
+        os.path.join(dataset_path, "data", subject_id))
 
     paths_label_subject_mask = []
     for k, v in annotations.items():
@@ -45,31 +54,46 @@ def load_files_for_subject(subject_id: str) -> list[tuple[str, str, str, list[in
             continue
         label = all_class_names[np.argmax(v[0][-num_classes:])]
         bounding_box = v[0][:4]
-        paths_label_subject_mask.append((dcm_path, label, subject_id, bounding_box))
+        paths_label_subject_mask.append(
+            (dcm_path, label, subject_id, bounding_box))
 
     return paths_label_subject_mask
 
 
 class LungPetCtDxDataset(Dataset):
     """Lung-PET-CT-Dx dataset."""
+
     color_channels = 3
 
-    def __init__(self, dataset_path: str = dataset_path, post_normalize_transform=None, cache=True, subject_count=None, exclude_classes: Union[list[str], None] = None, normalize=False, max_size: int = -1):
+    def __init__(
+        self,
+        dataset_path: str = dataset_path,
+        post_normalize_transform=None,
+        cache=True,
+        subject_count=None,
+        exclude_classes: Union[list[str], None] = None,
+        normalize=False,
+        max_size: int = -1,
+        crop_to_tumor: bool = False,
+        cropped_tumor_size=128
+    ):
         # dirs = [d for d in os.listdir(datasetPath) if os.isdir(d)]
         self.cache_file = Path(
-            f'../cache/{type(self).__name__}_metadata.pickle')
+            f"../cache/{type(self).__name__}_metadata.pickle")
         self.data_distribution_cache_file = Path(
-            f'../cache/{type(self).__name__}_dataset_distribution.pickle')
+            f"../cache/{type(self).__name__}_dataset_distribution.pickle" if not crop_to_tumor else f"../cache/{type(self).__name__}_cropped_dataset_distribution.pickle"
+        )
         self.exclude_classes = exclude_classes
         self.class_names = list(
-            filter(lambda item: not self.isExcluded(item), all_class_names))
-        csv_path = os.path.join(dataset_path, 'metadata.csv')
+            filter(lambda item: not self.isExcluded(item), all_class_names)
+        )
+        csv_path = os.path.join(dataset_path, "metadata.csv")
         csv_file = pd.read_csv(csv_path)
 
-        self.all_subjects = csv_file['Subject ID'].unique()
+        self.all_subjects = csv_file["Subject ID"].unique()
         self.filtered_subjects = None
         if subject_count:
-            print(f'Only using {subject_count} subjects')
+            print(f"Only using {subject_count} subjects")
             self.filtered_subjects = self.all_subjects[:subject_count]
 
         self.paths_label_subject_mask = []
@@ -78,6 +102,8 @@ class LungPetCtDxDataset(Dataset):
         self._disable_transform_and_norm = False
         self._force_normalize_for_dist_calc = False
         self._normalization_transform = None
+        self.crop_to_tumor = crop_to_tumor
+        self.cropped_resize_transform = transforms.Resize(cropped_tumor_size)
         self.load_metadata(cache, normalize)
 
         if max_size != -1:
@@ -88,7 +114,12 @@ class LungPetCtDxDataset(Dataset):
 
     def subject_split(self, test_size: float):
         subj_train, subj_test = train_test_split(
-            self.all_subjects if self.filtered_subjects == None else self.filtered_subjects, test_size=test_size, random_state=42)
+            self.all_subjects
+            if self.filtered_subjects == None
+            else self.filtered_subjects,
+            test_size=test_size,
+            random_state=42,
+        )
         idx_train = []
         idx_test = []
         for idx, t in enumerate(self.paths_label_subject_mask):
@@ -105,7 +136,7 @@ class LungPetCtDxDataset(Dataset):
     def get_class_weights_inverse(self):
         _, labels, _ = list(zip(*self.paths_label_subject_mask))
         labels = list(map(lambda l: self.to_one_hot(l), labels))
-        weights = np.sum(labels, axis=0, dtype='float32')
+        weights = np.sum(labels, axis=0, dtype="float32")
         weights = weights / weights.sum()
         weights = 1.0 / weights
         weights = weights / weights.sum()
@@ -113,7 +144,7 @@ class LungPetCtDxDataset(Dataset):
 
     def load_metadata(self, cache, normalize):
         if cache and os.path.exists(self.cache_file):
-            with open(self.cache_file, 'rb') as f:
+            with open(self.cache_file, "rb") as f:
                 self.paths_label_subject_mask = pickle.load(f)
         else:
             with ProcessPoolExecutor(max_workers=8) as executor:
@@ -122,29 +153,37 @@ class LungPetCtDxDataset(Dataset):
 
             if cache:
                 os.makedirs(self.cache_file.parent, exist_ok=True)
-                with open(self.cache_file, 'wb') as f:
+                with open(self.cache_file, "wb") as f:
                     # Pickle the 'data' dictionary using the highest protocol available.
-                    pickle.dump(self.paths_label_subject_mask,
-                                f, pickle.HIGHEST_PROTOCOL)
+                    pickle.dump(
+                        self.paths_label_subject_mask, f, pickle.HIGHEST_PROTOCOL
+                    )
         if not self.filtered_subjects == None:
             self.paths_label_subject_mask = list(
-                filter(lambda item: item[2] in self.filtered_subjects, self.paths_label_subject_mask))
+                filter(
+                    lambda item: item[2] in self.filtered_subjects,
+                    self.paths_label_subject_mask,
+                )
+            )
 
         if not self.exclude_classes == None:
             self.paths_label_subject_mask = list(
-                filter(lambda item: not self.isExcluded(item[1]), self.paths_label_subject_mask))
+                filter(
+                    lambda item: not self.isExcluded(item[1]),
+                    self.paths_label_subject_mask,
+                )
+            )
 
         if normalize:
             mean, sd = None, None
             if cache and os.path.exists(self.data_distribution_cache_file):
-                with open(self.data_distribution_cache_file, 'rb') as f:
+                with open(self.data_distribution_cache_file, "rb") as f:
                     mean, sd = pickle.load(f)
             else:
                 mean, sd = self.calculateMeanAndSD()
                 if cache:
-                    with open(self.data_distribution_cache_file, 'wb') as f:
-                        pickle.dump((mean, sd), f,
-                                    pickle.HIGHEST_PROTOCOL)
+                    with open(self.data_distribution_cache_file, "wb") as f:
+                        pickle.dump((mean, sd), f, pickle.HIGHEST_PROTOCOL)
             self._normalization_transform = transforms.Normalize(
                 mean=mean, std=sd)
 
@@ -172,13 +211,28 @@ class LungPetCtDxDataset(Dataset):
         # Convert to tensor with now proper Channel x Height x Width dimensions
         img = torch.from_numpy(img.astype(np.float32))
         # Normalize tumor bounding box before transformation
+
         _channels, sx, sy = img.shape
-        mask = np.array([mask[0] / sx, mask[1] / sy, mask[2] / sx, mask[3] / sy])
+
+        if self.crop_to_tumor:
+            xmin, ymin, xmax, ymax = mask
+            w, h = int(xmax-xmin), int(ymax-ymin)
+            if w == 0 and h == 0:
+                print("width and height of masked image are 0!")
+            img = transforms.functional.crop(
+                img, int(ymin), int(xmin), max(w, h), max(w, h))
+            img = self.cropped_resize_transform(img)
+            mask = np.array([0, 0, 1, 1])
+        else:
+            mask = np.array([mask[0] / sx, mask[1] / sy,
+                             mask[2] / sx, mask[3] / sy])
 
         if self._normalization_transform:
             img = self._normalization_transform(img)
 
-        if self.post_normalize_transform and (not self._disable_transform_and_norm or self._force_normalize_for_dist_calc):
+        if self.post_normalize_transform and (
+            not self._disable_transform_and_norm or self._force_normalize_for_dist_calc
+        ):
             img = self.post_normalize_transform(img)
 
         label_one_hot = self.to_one_hot(label)
@@ -189,16 +243,14 @@ class LungPetCtDxDataset(Dataset):
         if normalize:
             self._force_normalize_for_dist_calc = True
 
-        loader = DataLoader(self,
-                            batch_size=batch_size,
-                            num_workers=0,
-                            shuffle=False)
+        loader = DataLoader(self, batch_size=batch_size,
+                            num_workers=0, shuffle=False)
 
         mean = torch.zeros(self.color_channels)
         var = torch.zeros(self.color_channels)
 
         numBatches = len(loader)
-        for i, (images, _) in enumerate(loader):
+        for i, (images, _, _) in enumerate(loader):
             batch_mean = images.mean((2, 3)).transpose(0, 1).mean(1)
             mean += batch_mean
             width = images.size(3)
@@ -206,16 +258,26 @@ class LungPetCtDxDataset(Dataset):
             batch_size = images.size(0)
             color_channels = images.size(1)
 
-            batch_var = images.transpose(1, 3).sub(batch_mean).reshape((batch_size, width*height, color_channels)).square(
-            ).transpose(1, 2).transpose(0, 1).reshape((color_channels, batch_size*width*height)).mean(1)
+            batch_var = (
+                images.transpose(1, 3)
+                .sub(batch_mean)
+                .reshape((batch_size, width * height, color_channels))
+                .square()
+                .transpose(1, 2)
+                .transpose(0, 1)
+                .reshape((color_channels, batch_size * width * height))
+                .mean(1)
+            )
             var += batch_var
             print(
-                f'Calculating mean and sd of ds: {i}/{numBatches} batches done. ', end='\r')
+                f"Calculating mean and sd of ds: {i}/{numBatches} batches done. ",
+                end="\r",
+            )
 
-        print()
+        print(f"Done with {numBatches}")
         mean, sd = mean.div(numBatches), var.div(numBatches).sqrt()
 
-        print(f'Calculated mean: {mean}, sd: {sd}')
+        print(f"Calculated mean: {mean}, sd: {sd}")
 
         self._disable_transform_and_norm = False
         self._force_normalize_for_dist_calc = False
